1043426b67a81570504444b0740f19e29751e463
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-04-10 14:28:58 -0300

    bla1

--- src/controllers/example-controller.js
+++ src/controllers/example-controller.js
@@ -2,6 +2,10 @@ class ExampleRoute {
   index(req, res) {
     return res.status(200).json({ msg: "Hello World :)" });
   }
+
+  index1(req, res) {
+    return res.status(200).json({ msg: "Hello World :)" });
+  }
 }
 
 export default new ExampleRoute();
