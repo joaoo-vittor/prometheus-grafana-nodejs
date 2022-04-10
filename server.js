29b1a29f4c0a735fc0f944cb5d7f0ba181b91634
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-04-10 14:27:48 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+
+import app from "./app";
+
+const port = Number(process.env.PORT) || 3001;
+const host = process.env.HOST || "localhost";
+
+app.listen(port, host, () => {
+  console.log(`CTRL + Click em http://${host}:${port}`);
+});
